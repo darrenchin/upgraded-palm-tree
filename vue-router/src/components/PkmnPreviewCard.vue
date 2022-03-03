985f3eb11564b4bc0ea3602a50471d16c18c268a
darrenchin <90667359+darrenchin@users.noreply.github.com>
2022-03-03 13:04:03 -0500

    Initialize L3-Routing

---
+++ vue-router/src/components/PkmnPreviewCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <router-link to="/Test" class="router-link">
+    <div class="pkmn-container">
+      <h2>{{ index + 1 }}</h2>
+      <h1 id="query">{{ pokemon.name }}</h1>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: Object,
+    index: Number,
+  },
+};
+</script>
+
+<style  scoped>
+.router-link,
+.router-link:visited,
+.router-link:link {
+  width: 22%;
+  text-decoration: none;
+  background-color: red;
+  color: white;
+  margin: 1rem;
+  padding: 1rem 1rem;
+  border-radius: 10px;
+}
+h1 {
+  text-transform: uppercase;
+  font-size: var(--h3);
+}
+h2 {
+  font-size: var(--h4);
+}
+</style>
