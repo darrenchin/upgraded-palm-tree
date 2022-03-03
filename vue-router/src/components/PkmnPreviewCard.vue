dfb774f744bc8c049072cfd5bab3906a4efa1107
darrenchin <90667359+darrenchin@users.noreply.github.com>
2022-03-03 13:04:02 -0500

    Initialize L2-Props

---
+++ vue-router/src/components/PkmnPreviewCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="pkmn-container">
+    <h2>{{ index + 1 }}</h2>
+    <h1 id="query">{{ pokemon.name }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pokemon: Object,
+    index: Number,
+  },
+};
+</script>
+
+<style  scoped>
+div {
+  background-color: red;
+  color: white;
+  height: 10rem;
+  width: 22%;
+  border-radius: 10px;
+}
+h1 {
+  text-transform: uppercase;
+  font-size: var(--h3);
+}
+h2 {
+  font-size: var(--h4);
+}
+</style>
