ddcc6a0f93cf657f0cffb639cc664cce416d8180
darrenchin <90667359+darrenchin@users.noreply.github.com>
2022-03-03 13:04:03 -0500

    Initialize L5-ASync-Params

---
+++ vue-router/src/views/Pokemon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="poke-card">
+    <h2>{{ PokemonData.name }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      PokemonData: {},
+    };
+  },
+  beforeMount: function () {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function () {
+      try {
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.$route.params.id}`
+        );
+
+        const data = await response.json();
+        this.PokemonData = data;
+
+        return this.PokemonData;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
